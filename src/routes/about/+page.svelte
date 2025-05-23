a4fb3b9cb1f0c358306bf251484998473c086494
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2025-05-23 13:06:46 +0200

    Fixes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,3 +4,7 @@
 
 
 <a href="{base}/">Home</a>
+
+<p> Buongiorno! </p>
+<p> This is the about page. </p>
+<p> You can find the source code for this page in <code>src/routes/about/+page.svelte</code>. </p>
